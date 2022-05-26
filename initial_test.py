be8cc96a1911a57a9536196e7f16f8b7cfbd8530
SrGuilem <miguellevy8524@gmail.com>
2022-05-26 16:33:17 -0300

    API final

--- initial_test.py
+++ initial_test.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 from flask_restful import Api
-from resources.modulo import Modulo
+from resources.modulo import Modulo, ModuloExcel
 from resources.usuario import User, UserRegister, UserLogin, UserLogout
 from flask_jwt_extended import JWTManager
 from blocklist import BLOCKLIST
@@ -34,6 +34,7 @@ api.add_resource(User, '/usuarios/<int:user_id>')
 api.add_resource(UserRegister, '/cadastro')
 api.add_resource(UserLogin, '/login')
 api.add_resource(UserLogout, '/logout')
+api.add_resource(ModuloExcel, '/excel')
 
 if __name__ == '__main__':
     from sql_alchemy import banco
--- resources/modulo.py
+++ resources/modulo.py
@@ -1,7 +1,12 @@
+import requests
 from flask_restful import Resource, reqparse
 from models.modulo import ModuloModel
 from flask_jwt_extended import jwt_required
 import sqlite3
+import csv
+import pandas as pd
+import numpy as np
+import xlsxwriter
 
 
 class Modulo(Resource):
@@ -16,7 +21,7 @@ class Modulo(Resource):
         try:
             return {'modulos': [modulo.json() for modulo in ModuloModel.query.all()]}
         except:
-            return {'message': 'Server error.'}, 500 # not found
+            return {'message': 'Server error.'}, 500  # not found
 
     def post(self):
         dados = Modulo.argumentos.parse_args()
@@ -26,6 +31,50 @@ class Modulo(Resource):
         except:
             return {'message': 'Um erro interno ocrreu ao tentar salvar o módulo'}, 500  # Internal Server Error
         return modulo.json()
+
+
+class ModuloExcel(Resource):
+    def get(self):
+        """
+        Open a file to write the data
+        Connects to the database
+        Creates a cursor object to address the table results individually
+
+        :return: Created ok message
+        """
+        header = ['id', 'Corrente', 'Tensão', 'Data e Hora']
+
+        with open('/home/sr-guilem/PycharmProjects/projetoSolarWatcher/medicoes.csv', 'w') as write_file:
+            writer = csv.writer(write_file)
+            writer.writerow(header)
+            conn = sqlite3.connect('/home/sr-guilem/PycharmProjects/projetoSolarWatcher/banco.db')
+            cursor = conn.cursor()
+            for row in cursor.execute('SELECT * FROM modulos'):
+                writer.writerow(row)
+
+        read_file = pd.read_csv(r'/home/sr-guilem/PycharmProjects/projetoSolarWatcher/medicoes.csv')
+        read_file.to_excel(r'/home/sr-guilem/PycharmProjects/projetoSolarWatcher/medicoes.xlsx', index=False)
+
+        df = pd.read_excel('/home/sr-guilem/PycharmProjects/projetoSolarWatcher/medicoes.xlsx', 'Sheet1')
+        print(df)
+
+        pic = df.plot.line(title='Medições', x='Data e Hora', y=['Tensão', 'Corrente'], figsize=(15, 5), grid=True,
+                           legend=True, color={'Tensão': 'red', 'Corrente': 'blue'})
+        pic = pic.get_figure()
+        pic.savefig('grph.jpg')
+
+        afk = '/home/sr-guilem/PycharmProjects/projetoSolarWatcher/medicoes.xlsx'
+        workbook = xlsxwriter.Workbook(afk)
+        sheet = workbook.add_worksheet()
+        sheet.insert_image('G3', 'grph.jpg')
+
+        for i, col_name in enumerate(df.columns):
+            sheet.write(0, i, col_name)
+            sheet.write_column(1, i, df[col_name])
+
+        workbook.close()
+
+        return {'message': 'Documento criado com sucesso'}
 
     @jwt_required()
     def put(self, modulo_id):
